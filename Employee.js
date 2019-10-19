f2ee6f4d265e053d6de9b47155829e9275f28d22
Gary <g4ryzhang@gmail.com>
2019-10-19 15:10:23 -0400

    Employee "class"

---
+++ Employee.js
@@ -0,0 +1,45 @@
+'use strict';
+
+let allEmployees = []
+
+
+class Employee {
+  constructor(name,  password, position, availability) {
+    this.name = name
+    this.userID = allEmployees.length
+    this.password = password
+    this.position = position
+    this.availability = availability
+  }
+}
+
+class TimeInterval {
+  constructor(start, end) {
+    this.start = start
+    this.end = end
+    this.duration = end - start
+  }
+}
+
+// returns true if TimeInterval i1 contains i2, false otherwise
+function compareIntervals(i1, i2) {
+  return (i1.start <= i2.start && i1.end >= i2.end)
+}
+
+
+
+
+
+// example employees
+const aliceDailyAvail = new TimeInterval(8, 15)
+const bobDailyAvail = new TimeInterval(9, 16)
+let aliceWeeklyAvail = []
+let bobWeeklyAvail = []
+
+for (let i = 0; i < 7; i++) {
+  aliceWeeklyAvail.push(aliceDailyAvail)
+  bobWeeklyAvail.push(bobDailyAvail)
+}
+
+allEmployees.push(new Employee('Alice', '123', 'waitress', aliceWeeklyAvail))
+allEmployees.push(new Employee('Bob', '123', 'cook', bobWeeklyAvail))
