721198951260110db95d0c5017aeb88ae89b1c11
Gary <g4ryzhang@gmail.com>
2019-10-19 15:11:43 -0400

    TimeAvail page first attempt

--- TimeAvail.js
+++ TimeAvail.js
@@ -4,7 +4,7 @@ let allEmployees = []
 
 
 class Employee {
-  constructor(name,  password, position, availability) {
+  constructor(name, password, position, availability) {
     this.name = name
     this.userID = allEmployees.length
     this.password = password
@@ -26,23 +26,43 @@ function compareIntervals(i1, i2) {
   return (i1.start <= i2.start && i1.end >= i2.end)
 }
 
-
-
-
-
-// example employees
 const aliceDailyAvail = new TimeInterval(8, 15)
 const bobDailyAvail = new TimeInterval(9, 16)
 let aliceWeeklyAvail = []
 let bobWeeklyAvail = []
 
-// the array is of length 7 which corresponds to the available time for
-// each day in a week (from Monday to Sunday)
-
-for (let i = 0; i < 7; i++) {
+for (let i = 0; i < 5; i++) {
   aliceWeeklyAvail.push(aliceDailyAvail)
   bobWeeklyAvail.push(bobDailyAvail)
 }
 
 allEmployees.push(new Employee('Alice', '123', 'waitress', aliceWeeklyAvail))
 allEmployees.push(new Employee('Bob', '123', 'cook', bobWeeklyAvail))
+
+
+
+const availTable = document.querySelector('#AvailabilityTable')
+
+
+
+availTable.addEventListener('click', updateAvail)
+
+function updateAvail() {
+  e.preventDefault()
+
+
+  if (e.target.classList.contains('dayCell')) {
+    let start = askStart()
+    let end = askEnd()
+
+  }
+}
+
+function askStart() {
+  let start = promp("Please enter start time", "")
+  if (start == null) {
+    askStart()
+  }
+  else
+    return start
+}
