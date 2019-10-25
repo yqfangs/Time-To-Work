25ffc2610961467164ac8bd1eb2697d955755c43
Xinchen Cai <xinchen.cai@mail.utoronto.ca>
2019-10-25 01:49:53 -0400

    add new function to caculate the total hours

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,28 @@
 'use strict';
+const log = console.log;
+
+function CheckTotalHour(){
+	var total = 0;
+	if(document.getElementById("MondayHour").innerText != ""){
+		total += parseInt(document.getElementById("MondayHour").innerText)
+	}
+	if(document.getElementById("TuesdayHour").innerText != ""){
+		total += parseInt(document.getElementById("TuesdayHour").innerText)
+	}
+	if(document.getElementById("WednesdayHour").innerText != ""){
+		total += parseInt(document.getElementById("WednesdayHour").innerText)
+	}
+	if(document.getElementById("ThursdayHour").innerText != ""){
+		total += parseInt(document.getElementById("ThursdayHour").innerText)
+	}
+	if(document.getElementById("FridayHour").innerText != ""){
+		total += parseInt(document.getElementById("FridayHour").innerText)
+	}
+	if(document.getElementById("SaturdayHour").innerText != ""){
+		total += parseInt(document.getElementById("SaturdayHour").innerText)
+	}
+	if(document.getElementById("SundayHour").innerText != ""){
+		total += parseInt(document.getElementById("SundayHour").innerText)
+	}
+	document.write(total + "h");
+}
