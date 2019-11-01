dd125730532ae839dc8ee6179c0e3ea1666158fc
Yun Qi Fang <yunqi.fang@mail.utoronto.ca>
2019-11-01 16:36:16 -0400

    modify dashboard and link employer with dashboard

--- Employer.js
+++ Employer.js
@@ -1,22 +1,11 @@
 'use strict';
 
-const allEmployers = []
-const allEmployees = []
-
-class TimeInterval {
-  constructor(start, end) {
-    this.start = start
-    this.end = end
-    this.duration = end - start
-  }
-}
-
 class Employer {
 	constructor(name, password, email, companyName){
 		this.name = name
+		this.password = password
 		this.userID = allEmployers.length
 		this.email = email
-		this.password = password
 		this.companyName = companyName
 	}
 }
