3076d41f12baf3b08a3ec512f9e35ca026c2cba2
Xinchen Cai <xinchen.cai@mail.utoronto.ca>
2019-11-02 21:23:31 -0400

    change a order of codes

--- MockData.js
+++ MockData.js
@@ -9,13 +9,19 @@ const dayId = ['MondayTime', 'TuesdayTime','WednesdayTime', 'ThursdayTime', 'Fri
 // company1 employees: Alice, Bob, Caitlyn, Darius
 
 const alice = new Employee('Alice', '123', 'alice@mail.com', 'waitress', '121345678', 'company1');
+allEmployees.push(alice);
 const bob = new Employee('Bob', '123', 'bob@mail.com', 'cook', '123456798', 'company2');
+allEmployees.push(bob);
 const caitlyn = new Employee('Caitlyn', '123', 'caitlyn@mail.com', 'cook', '1236879089', 'company1');
+allEmployees.push(caitlyn);
 const darius = new Employee('Darius', '123', 'darius@mail.com', 'supervisor', '7896751673', 'company1');
+allEmployees.push(darius);
 
 const employer1 = new Employer('employer1', '123', 'employer1@mail.com', '123567989', 'company1');
+allEmployers.push(employer1);
 
 const user = new Employee('user', 'user', 'user', 'user', '6477777777', 'company2');
+allEmployees.push(user);
 
 
 allCompanies.push(new Company('company1', new TimeInterval(8, 24)))
@@ -23,14 +29,7 @@ allCompanies.push(new Company('company2', new TimeInterval(9, 22)))
 
 
 
-allEmployees.push(alice);
-allEmployees.push(bob);
-allEmployees.push(caitlyn);
-allEmployees.push(darius);
 
-allEmployers.push(employer1);
-
-allEmployees.push(user);
 
 
 alice.getCompany().employees.push(alice);
