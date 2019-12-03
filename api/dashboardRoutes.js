1bb072167c2c60b285dfbd034a3acfddf9cba3cc
Yun Qi Fang <yunqi.fang@mail.utoronto.ca>
2019-12-03 02:12:00 -0500

    dashboard employer

--- api/dashboardRoutes.js
+++ api/dashboardRoutes.js
@@ -2,6 +2,7 @@ const log = console.log;
 const express = require('express');
 const router = express.Router();
 const { Employee } = require('../db/models/employee')
+const { Employer } = require('../db/models/employer')
 const { ObjectID } = require('mongodb')
 
 router.get('/', (req, res) =>{
@@ -10,21 +11,36 @@ router.get('/', (req, res) =>{
         if(!ObjectID.isValid(id)){
             res.status(404).send()
         }
-
-        Employee.findById(id).then((employee) =>{
+        
+        if(req.session.type == "employee"){
+            Employee.findById(id).then((employee) =>{
             if(!employee){
                 res.status(404).send()
             } else{
-                res.send(employee)
+                const type = "employee"
+                res.send({ employee, type })
             }
-        }).catch((error)=>{
-            res.status(500).send()
-        })
+            }).catch((error)=>{
+                res.status(500).send()
+            })
+        }else if(req.session.type == "employer"){
+            Employer.findById(id).then((employer) =>{
+            if(!employer){
+                res.status(404).send()
+            } else{
+                const type = "employer"
+                res.send({employer, type})
+            }
+            }).catch((error)=>{
+                res.status(500).send()
+            })
+        }
     }else{
         res.redirect('/login');
     }
 
 })
+
 
 
 module.exports = router;
