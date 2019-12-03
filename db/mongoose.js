cb27ab95389b3e160e98a13c50000d24f91874b6
Yun Qi Fang <yunqi.fang@mail.utoronto.ca>
2019-12-03 18:34:58 -0500

    a

--- db/mongoose.js
+++ db/mongoose.js
@@ -5,8 +5,8 @@ const mongoose = require('mongoose')
 
 /* Connnect to our database */
 // Get the URI of the local database, or the one specified on deployment.
-const mongoURI = process.env.MONGODB_URI || 'mongodb://cara:981215@cluster0-shard-00-00-xcwjq.mongodb.net:27017,cluster0-shard-00-01-xcwjq.mongodb.net:27017,cluster0-shard-00-02-xcwjq.mongodb.net:27017/project39?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
-// const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/project'
+//const mongoURI = process.env.MONGODB_URI || 'mongodb://cara:981215@cluster0-shard-00-00-xcwjq.mongodb.net:27017,cluster0-shard-00-01-xcwjq.mongodb.net:27017,cluster0-shard-00-02-xcwjq.mongodb.net:27017/project39?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
+ const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/project'
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false});
 
